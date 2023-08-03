2f04e467b5f853e8a3210942f10c46d0fe8fa289
black-dotcom <62455479+black-dotcom@users.noreply.github.com>
2023-08-03 10:14:03 +0800

    Create config.py
    配置文件

---
+++ config.py
@@ -0,0 +1,23 @@
+
+User = "root"
+Password = "root"
+Ip = "127.0.0.1"
+Database = "usdt"
+
+# 管理员tgID
+Admin_id = "1707841429"
+# 管理员用户名
+Admin_name = "toumingde"
+# 群组ID
+Group_id = "-1001948357949"
+# 群组用户名
+Group_name = "cesiyongbao"
+# 频道用户名
+Channel_name = "yingsheng001"
+# 拉新奖励 10 对应0.1U
+New_reward = 10
+# 红包倍数
+Bei = 1.8
+# 红包数量
+Num = 2
+
